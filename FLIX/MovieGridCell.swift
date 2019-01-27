5ca0e50a9728f819af410e9d6581002b1b2c9749
Pratyaksh99 <31634863+Pratyaksh99@users.noreply.github.com>
2019-01-27 18:28:08 -0500

    Final Commit

---
+++ FLIX/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  FLIX
+//
+//  Created by Pratyaksh Motwani on 1/22/19.
+//  Copyright © 2019 Pratyaksh Motwani. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
